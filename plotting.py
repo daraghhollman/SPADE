c01380d46360a22230468cc92f9f3a05608b2f78
Daragh Hollman <hollmandaragh@gmail.com>
2024-06-05 11:29:34 +0100

    Put some more thougths towards file structure and useability. Implemented multipanel class. Data class will contain nessesary information to plot it

--- plotting.py
+++ plotting.py
@@ -1,3 +1,20 @@
+import matplotlib.pyplot as plt
+import dataProcessing
+
 class Panel:
-    def __init__(self, mission: str) -> None:
-        self.mission = mission
+    def __init__(self, data: Data) -> None:
+        self.data = data
+
+
+class Multipanel:
+    def __init__(self, panels: list, panelIndices: list) -> None:
+        """Panels contains a list of panel classes to be plotted at positions governed by panelIndices."""
+        self.panels = panels
+        self.panelIndices = panelIndices
+
+    def Plot(self):
+        fig, axes = plt.subplots(1, len(self.panels))
+
+        for panel, panelIndex in zip(self.panels, self.panelIndices):
+           
+            panel.data.Plot(fig, axes[panelIndex])
